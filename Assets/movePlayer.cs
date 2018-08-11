b70d48eff1cecdffdcd5e3115a2ae1af56b5344d
john <contemplator1998@gmail.com>
2018-08-11 14:38:29 +0300

    Added LOL

--- Assets/movePlayer.cs
+++ Assets/movePlayer.cs
@@ -3,17 +3,21 @@ using System.Collections;
 
 public class movePlayer : MonoBehaviour {
 
+    public Rigidbody rb;
+
 	// Use this for initialization
 	void Start () {
-	
+        rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
+    void Update()
+    {
         var cursor = GameObject.Find("Cursor");
         var normalized = Vector3.Normalize(cursor.transform.position - transform.position);
         var delta = Vector3.Scale(normalized, new Vector3(0.1F, 0.1F));
-        transform.position += delta;
-
-	}
+        //transform.position += delta;
+        // LOL
+        rb.AddForce(delta);
+    }
 }
