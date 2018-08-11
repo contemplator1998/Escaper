0fb1dedc6945d5c7f320fcb3ba86d918e0e1170a
john <contemplator1998@gmail.com>
2018-08-11 14:09:03 +0300

    Added moving player

--- Assets/moveCursor.cs
+++ Assets/moveCursor.cs
@@ -11,7 +11,6 @@ public class moveCursor : MonoBehaviour {
      
      // Update is called once per frame
      void Update () {
-
          var distance_to_screen = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
          var pos_move = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance_to_screen));
          transform.position = new Vector3(pos_move.x, pos_move.y, pos_move.z);
--- Assets/movePlayer.cs
+++ Assets/movePlayer.cs
@@ -10,6 +10,10 @@ public class movePlayer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        var cursor = GameObject.Find("Cursor");
+        var normalized = Vector3.Normalize(cursor.transform.position - transform.position);
+        var delta = Vector3.Scale(normalized, new Vector3(0.1F, 0.1F));
+        transform.position += delta;
+
 	}
 }
